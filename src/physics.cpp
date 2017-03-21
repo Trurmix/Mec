e0377b94f47105c0e7771fa1b97110ffeec84ba8
lprovencio <lprovencio@S5-07>
2017-03-21 11:21:03 +0100

    hola

--- src/physics.cpp
+++ src/physics.cpp
@@ -9,11 +9,14 @@
 #include <time.h>
 #include <iostream>
 
-float *partVerts;
 bool show_test_window = false;
 bool euler = true;
 int waterfallIncrementX = -3;
 float timePerFrame = 0.033;
+float lenght = 0.5;
+glm::vec3 gravity = glm::vec3(0, -9.8, 0);
+int maxMesh = 252;
+glm::vec3 normal;
 
 void GUI() {
 	{	//FrameRate
@@ -50,25 +53,68 @@ struct Particle {
 	glm::vec3 lastVel;
 };
 
-float *arra = new float[3*252];
+float *arra = new float[3*maxMesh];
 Particle *particlesContainer = new Particle[LilSpheres::maxParticles];
 
 void PhysicsInit() {
 
+	//Grid
 	for (int i = 0; i < LilSpheres::maxParticles; ++i) {
-
-		arra[3 * i + 0] = particlesContainer[i].pos.x = -5 + 0.5 * (i % 14);
-		arra[3 * i + 1] = particlesContainer[i].pos.y = 10 - 0.5 * (i / 14);
+		//particlesContainer[i].pos = glm::vec3(arra[i * 3], arra[i * 3 + 1], arra[i * 3 + 2]) = glm::vec3(-5 + lenght * (i % 14) , 10 - lenght * (i / 14), 0);
+		
+		arra[3 * i + 0] = particlesContainer[i].pos.x = -5 + lenght * (i % 14);
+		arra[3 * i + 1] = particlesContainer[i].pos.y = 10 - lenght * (i / 14);
 		arra[3 * i + 2] = particlesContainer[i].pos.z = 0;
-	}
 
+		particlesContainer[i].vel = glm::vec3(0.f, 0.f, 0.f);
+		
+	}
 	ClothMesh::updateClothMesh(arra);
+
+	srand(time(NULL));
+
+	
+
 }
 
 void PhysicsUpdate(float dt) {
+
+	glm::vec3 temp;
+
+	//euler
+	for (int i = 0; i < maxMesh; i++) 
+	{
+		particlesContainer[i].pos = particlesContainer[i].pos + particlesContainer[i].vel *dt;
+		particlesContainer[i].vel = particlesContainer[i].vel + gravity *dt;
+
+		arra[3 * i + 0] = particlesContainer[i].pos.x;
+		arra[3 * i + 1] = particlesContainer[i].pos.y;
+		arra[3 * i + 2] = particlesContainer[i].pos.z;
+	}
+
+	ClothMesh::updateClothMesh(arra);
+	//colisions
+
+	//pla sostre
+		
+	//pla terra
+	normal = glm::vec3(0, 1, 0);
+	for (int i = 0; i < maxMesh; i++)
+	{
+		//if(particlesContainer[i].pos.y <= 0)
+		if (particlesContainer[i].pos.y <= 0) {
+			
+		}
+		
+	}
+	//pla lateral esquerra
+	
+	//pla lateral dret
+
+	//spring 1-dimensional model
 	//TODO
 }
 void PhysicsCleanup() {
 	//TODO
-	delete[] partVerts;
+	delete[] arra;
 }
